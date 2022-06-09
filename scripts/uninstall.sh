1c93a500f44a74489a839cc085febd551a36ed5d
Christoph Schöning <schoning.christoph@gmail.com>
2022-06-09 13:20:59 +0200

    added remove script

---
+++ scripts/uninstall.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+KLIPPER_PATH="${HOME}/klipper"
+SYSTEMDDIR="/etc/systemd/system"
+
+# helper functions
+verify_ready()
+{
+    if [ "$EUID" -eq 0 ]; then
+        echo "This script must not run as root"
+        exit -1
+    fi
+}
+
+# link the extension
+unlink_extension()
+{
+    echo "Unlinking extension to Klipper..."
+    rm "${KLIPPER_PATH}/klippy/extras/probe_z_offset_calibrate.py"
+}
+
+# restarting Klipper
+restart_klipper()
+{
+    echo "Restarting Klipper..."
+    sudo systemctl restart klipper
+}
+
+# Force script to exit if an error occurs
+set -e
+
+# Find SRCDIR from the pathname of this script
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
+
+# Parse command line arguments
+while getopts "k:" arg; do
+    case $arg in
+        k) KLIPPER_PATH=$OPTARG;;
+    esac
+done
+
+# Run steps
+verify_ready
+unlink_extension
+restart_klipper
